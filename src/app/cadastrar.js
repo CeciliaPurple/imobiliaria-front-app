031dffe3d75c5ecc5bcf678d937ed27edcd103e5
Maria Cecília <o.cecilia@aluno.ifsp.edu.br>
2025-09-23 17:48:41 -0300

    Página Cadastrar iniciada

---
+++ src/app/cadastrar.js
@@ -0,0 +1,82 @@
+import { Text, StyleSheet, ImageBackground, View, TextInput } from "react-native"
+import { Image } from "expo-image"
+import { Link } from "expo-router"
+
+
+
+export default function Home() {
+    return (
+        <ImageBackground style={styles.container} source={require('../../assets/img/gradient2.png')} resizeMode="stretch">
+            <Image style={styles.logo} source={require('../../assets/img/villa-logo-img.png')} />
+
+            <View>
+                <Text>Cadastro</Text>
+                <View>
+                    <TextInput />
+                </View>
+            </View>
+
+            <View style={styles.container_group}>
+                <Link href={'/'} style={styles.link}>
+                    <Text style={styles.btn}>Cadastrar</Text>
+                </Link>
+
+                <Text style={styles.text}>Já possui uma conta?  <Link href={'/'} style={styles.link_text}>Faça Login!</Link></Text>
+            </View>
+
+        </ImageBackground>
+
+
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 50,
+    },
+    container_logo: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 100
+    },
+    text_logo: {
+        color: '#146FBA',
+        width: 250,
+        textAlign: 'center'
+    },
+    text: {
+        color: '#375A76'
+    },
+    logo: {
+        width: 75,
+        height: 75
+    },
+    container_group: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 20
+    },
+    link: {
+        width: 'fit-content'
+    },
+    btn: {
+        backgroundColor: '#146FBA',
+        color: '#F2F1F6',
+        paddingHorizontal: 60,
+        paddingVertical: 10,
+        borderRadius: 10,
+        fontSize: 16,
+        fontWeight: 500
+    },
+    link_text: {
+        fontWeight: 'bold',
+        color: '#146FBA'
+    }
+})
